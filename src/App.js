a4e93361d01ef65297749fc106653a5708a899ea
Tho Chau <chauphuoctho2006@gmail.com>
2026-04-28 22:50:13 +0700

    Basic logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [todo, setTodo] = useState([]);
+  function handleAdd(value) {
+    const newTodo = {
+      id: Date.now(),
+      text: value,
+      completed: false,
+    };
+    setTodo([...todo, newTodo]);
+  }
+  function handleDelete(value) {
+    const x = todo.filter((todo) => todo.id !== value);
+    setTodo(x);
+  }
+  function handleToggle(value) {
+    const y = todo.map((item) =>
+      item.id === value ? { ...item, completed: !item.completed } : item,
+    );
+    setTodo(y);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoInput onHandleAdd={handleAdd} />
+      <TodoList
+        todo={todo}
+        onHandleDelete={handleDelete}
+        onHandleToggle={handleToggle}
+      />
     </div>
   );
 }
 
-export default App;
+function TodoInput({ onHandleAdd }) {
+  const [todoInput, setTodoInput] = useState("");
+
+  return (
+    <div className="todo-input">
+      <input
+        type="text"
+        value={todoInput}
+        onChange={(e) => setTodoInput(e.target.value)}
+      />
+      <button onClick={() => onHandleAdd(todoInput)}>Add</button>
+    </div>
+  );
+}
+
+function TodoList({ todo, onHandleDelete, onHandleToggle }) {
+  return (
+    <div>
+      <ul>
+        {todo.map((item) => (
+          <TodoItem
+            todo={item}
+            key={item.id}
+            onHandleDelete={onHandleDelete}
+            onHandleToggle={onHandleToggle}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function TodoItem({ todo, onHandleDelete, onHandleToggle }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => onHandleToggle(todo.id)}
+      />
+      ;<span>{todo.text}</span>
+      <button onClick={() => onHandleDelete(todo.id)}>X</button>
+    </li>
+  );
+}
